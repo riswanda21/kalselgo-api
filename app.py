2aeb5329982360c5a411234cf374df263590cbbe
riswanda21 <albertcastell12@gmail.com>
2022-11-23 08:29:59 +0800

    Update

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ from tinydb import TinyDB, Query
 app = Flask(__name__)
 wisataDB = TinyDB("data/wisata.json")
 
-@app.route("/")
+@app.route("/", methods = ["GET"])
 def home():
     return jsonify({"status":200})
 
-@app.route("/api/wisata/")
+@app.route("/api/wisata/", methods = ["GET"])
 def wisata_all():
     return jsonify({"status":"200",
                     "wisata":wisataDB.all()})
 
-@app.route("/api/wisata/<string:location>/")
+@app.route("/api/wisata/<string:location>/", methods = ["GET"])
 def wisata(location):
     data = Query()
     return jsonify({"status":"200",
